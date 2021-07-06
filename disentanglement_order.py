dcf746cfc35f443ccf264832f57874aca08cb3ef
clouderow <clouderow@gmail.com>
2021-07-06 12:11:39 +0800

    dataset

--- disentanglement_order.py
+++ disentanglement_order.py
@@ -13,20 +13,24 @@ args = parser.parse_args()
 
 seed = h.sweep('train.random_seed', h.discrete(range(args.s, args.e)))
 
-model = h.sweep('model.regularizers', h.discrete(['[@vae]', '[@beta_tc_vae]', '[@cascade_vae_c]']))
+model = h.sweep('model.regularizers', h.discrete(['[@vae]', '[@beta_tc_vae]']))
+model.append({
+    'model.regularizers': '[@cascade_vae_c]',
+    'cascade_vae_c.stage_steps': '5000',
+})
 dataset = [
-    {'train.dataset': "\"'dsprites_full'\"", },
+    {'train.dataset': "\"'dsprites_full'\"", 'dataset.name': "\"'dsprites_full'\"",},
     # {'train.dataset': "\"'color_dsprites'\"", },
     # {'train.dataset': "\"'scream_dsprites'\"",},
-    {'train.dataset.': "\"'smallnorb'\"",},
-    {'train.dataset.': "\"'cars3d'\"",},
+    {'train.dataset': "\"'smallnorb'\"",'dataset.name': "\"'smallnorb'\"",},
+    {'train.dataset': "\"'cars3d'\"",'dataset.name': "\"'cars3d'\""},
 ]
 runs = h.product([seed, model, dataset])
 
 general_config = {
     'train.eval_callbacks': '[@eval_mig]',
     'eval_mig.evaluation_steps': 1000,
-    'train.training_steps': '50000'
+    'train.training_steps': '50000',
 }
 metrics = " --metrics dci factor_vae_metric modularity_explicitness"
 print(len(runs))
