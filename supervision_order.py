984fb42b97faf41db7cd01e66918858e01e59261
clouderow <clouderow@gmail.com>
2021-07-09 00:06:20 +0800

    supervision_order.py cmds

--- supervision_order.py
+++ supervision_order.py
@@ -1,29 +1,29 @@
 import os
 import disentanglement_lib.utils.hyperparams as h
 import argparse
-
+import numpy as np
 from disentanglement_lib.methods.unsupervised import train
 from disentanglement_lib.methods.unsupervised.model import Regularizer
-
+os.environ['WANDB_ENTITY'] = 'dlib'
 os.environ['WANDB_PROJECT'] = 'uncertainty'
 os.environ['WANDB_TAGS'] = 'supervision'
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+# os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 import gin
 from pytorch_lightning.callbacks import Callback
 import matplotlib.pyplot as plt
 import torch
-import numpy as np
-import torchvision.utils
 import wandb
-from disentanglement_lib.methods.unsupervised.train import Train
+from disentanglement_lib.data.ground_truth.named_data import get_named_ground_truth_data
 
-from disentanglement_lib.methods.shared.architectures import View
-from pytorch_lightning.loggers import WandbLogger
-import pytorch_lightning as pl
 from torch import nn
 import torch.nn.functional as F
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str)
+parser.add_argument('--seed', type=int, default=0)
+args = parser.parse_args()
 
+data = get_named_ground_truth_data(args.dataset)
 
 @gin.configurable("supervision")
 class Supervision(Regularizer):
@@ -35,8 +35,8 @@ class Supervision(Regularizer):
         self.beta = beta
         self.steps = steps
 
-        mean = torch.tensor([1.5, 11.5, 91.0])
-        std = torch.tensor([1.118033988749895, 6.922186552431729, 52.82676089508675])
+        mean = torch.tensor([ np.mean(range(i)) for i in data.factors_num_values])
+        std = torch.tensor([ np.std(range(i)) for i in data.factors_num_values])
         self.register_buffer('mean', mean)
         self.register_buffer('std', std)
 
@@ -46,13 +46,11 @@ class Supervision(Regularizer):
         if stage < len(self.order):
             _, labels = data_batch
             factors = (labels-self.mean) / self.std
-            l_sup=F.mse_loss(z_mean[:,stage], factors[:,self.order[stage]])
+            l_sup=F.mse_loss(z_sampled[:,stage], factors[:,self.order[stage]])
             model.summary['supervision'] = l_sup.item()
             return l_sup * self.beta
         else:
             return 0
-
-# gin.enter_interactive_mode()
 
 gin_bindings = ['vae.beta=6',
                 'supervision.beta=10',
@@ -61,11 +59,11 @@ gin_bindings = ['vae.beta=6',
                 'model.encoder_fn = @conv_encoder',
                 'model.decoder_fn = @deconv_decoder',
                 'reconstruction_loss.loss_fn = @bernoulli_loss',
-                "train.dataset='cars3d'",
-                "dataset.name='cars3d'",
+                f"train.dataset='{args.dataset}'",
+                f"dataset.name='{args.dataset}'",
                 'train.training_steps=5000',
                 'train.eval_callbacks=[@eval_mig]',
-                'train.random_seed=99',
+                f'train.random_seed={args.seed}',
                 'train.batch_size=256',
                 'eval_mig.evaluation_steps=50',
                 'discretizer.discretizer_fn = @histogram_discretizer',
@@ -74,7 +72,10 @@ gin_bindings = ['vae.beta=6',
                 ]
 
 from itertools import permutations
-for order in permutations(range(3)):
-    order_binding=[f'supervision.order={order[:2]}']
-    train.train_with_gin('tmp',True,None,gin_bindings=gin_bindings+order_binding)
-
+for order in permutations(range(data.num_factors)):
+    order_binding=[f'supervision.order={order[:data.num_factors-1]}']
+    try:
+        train.train_with_gin('tmp',True,None,gin_bindings=gin_bindings+order_binding)
+        wandb.join()
+    except :
+        pass
