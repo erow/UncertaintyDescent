da3dcfac445d2e644d8192c848fc9330aa3657f3
clouderow <clouderow@gmail.com>
2021-07-09 10:27:10 +0800

    supervision_order.py cmds

--- supervision_order.py
+++ supervision_order.py
@@ -2,8 +2,10 @@ import os
 import disentanglement_lib.utils.hyperparams as h
 import argparse
 import numpy as np
-from disentanglement_lib.methods.unsupervised import train
+from disentanglement_lib.methods.unsupervised.train import Train, pl
 from disentanglement_lib.methods.unsupervised.model import Regularizer
+from pytorch_lightning.loggers import WandbLogger
+
 os.environ['WANDB_ENTITY'] = 'dlib'
 os.environ['WANDB_PROJECT'] = 'uncertainty'
 os.environ['WANDB_TAGS'] = 'supervision'
@@ -18,6 +20,7 @@ from disentanglement_lib.data.ground_truth.named_data import get_named_ground_tr
 
 from torch import nn
 import torch.nn.functional as F
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str)
 parser.add_argument('--seed', type=int, default=0)
@@ -25,36 +28,40 @@ args = parser.parse_args()
 
 data = get_named_ground_truth_data(args.dataset)
 
+
 @gin.configurable("supervision")
 class Supervision(Regularizer):
     """AnnealedVAE model."""
 
-    def __init__(self, order=gin.REQUIRED, beta =1, steps=1000):
+    def __init__(self, order=gin.REQUIRED, beta=1, steps=1000):
         super().__init__()
         self.order = order
         self.beta = beta
         self.steps = steps
 
-        mean = torch.tensor([ np.mean(range(i)) for i in data.factors_num_values])
-        std = torch.tensor([ np.std(range(i)) for i in data.factors_num_values])
+        mean = torch.tensor([np.mean(range(i)) for i in data.factors_num_values])
+        std = torch.tensor([np.std(range(i)) for i in data.factors_num_values])
         self.register_buffer('mean', mean)
         self.register_buffer('std', std)
+        print(self.mean,self.std)
 
     def forward(self, data_batch, model, kl, z_mean, z_logvar, z_sampled):
 
         stage = (model.global_step // self.steps)
         if stage < len(self.order):
+
             _, labels = data_batch
-            factors = (labels-self.mean) / self.std
-            l_sup=F.mse_loss(z_sampled[:,stage], factors[:,self.order[stage]])
+            factors = (labels - self.mean) / self.std
+            l_sup = F.mse_loss(z_sampled[:, stage], factors[:, self.order[stage]])
             model.summary['supervision'] = l_sup.item()
             return l_sup * self.beta
         else:
             return 0
 
+
 gin_bindings = ['vae.beta=6',
                 'supervision.beta=10',
-                'model.regularizers=[@vae,@supervision]',
+                'model.regularizers=[@vae, @supervision]',
                 'model.num_latent = 10',
                 'model.encoder_fn = @conv_encoder',
                 'model.decoder_fn = @deconv_decoder',
@@ -72,10 +79,20 @@ gin_bindings = ['vae.beta=6',
                 ]
 
 from itertools import permutations
+
 for order in permutations(range(data.num_factors)):
-    order_binding=[f'supervision.order={order[:data.num_factors-1]}']
+    order_binding = [f'supervision.order={order[:data.num_factors - 1]}']
     try:
-        train.train_with_gin('tmp',True,None,gin_bindings=gin_bindings+order_binding)
-        wandb.join()
-    except :
+        logger = WandbLogger()
+        pl_model = Train()
+        trainer = pl.Trainer(logger,
+                             progress_bar_refresh_rate=500,  # disable progress bar
+                             max_steps=pl_model.training_steps,
+                             checkpoint_callback=False,
+                             gpus=1,)
+        trainer.fit(pl_model)
+        pl_model.save_model('model.pt', 'tmp')
+    except:
         pass
+    finally:
+        wandb.join()
---
+++ train_supervision.sh
@@ -0,0 +1,11 @@
+#python supervision_order.py --dataset cars3d --seed=0
+python supervision_order.py --dataset dsprites_full --seed=0
+python supervision_order.py --dataset smallnorb --seed=0
+
+python supervision_order.py --dataset cars3d --seed=1
+python supervision_order.py --dataset dsprites_full --seed=1
+python supervision_order.py --dataset smallnorb --seed=1
+
+python supervision_order.py --dataset cars3d --seed=2
+python supervision_order.py --dataset dsprites_full --seed=2
+python supervision_order.py --dataset smallnorb --seed=2
